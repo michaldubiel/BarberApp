ceb3df2aa99436b416996ed9a4e7dbdbc367d971
michaldubiel <michal.a.dubiel@gmail.com>
2016-02-21 21:17:33 +0100

    Fixed activitiesController to populate activity edit view with proper data

--- src/Barber/Scripts/Controllers/activitiesController.js
+++ src/Barber/Scripts/Controllers/activitiesController.js
@@ -20,13 +20,9 @@
         $scope.activity = new Activity();
         $scope.time = null;
         $scope.employees = Employee.query();
-        console.log($scope.employees);
 
         $scope.add = function () {
-            console.log($scope.activity);
-            console.log($scope.time);
-            this.activity.Date.setHours(this.time.split(':')[0]);
-            this.activity.Date.setMinutes(this.time.split(':')[1]);
+            this.activity.Date.setUTCHours($scope.time.split(':')[0], $scope.time.split(':')[1]);
             $scope.activity.$save(function () {
                 $location.path('/');
             });
@@ -37,10 +33,6 @@
 
     function ActivitiesEditController($scope, $routeParams, $location, Activity, Employee) {
         $scope.activity = Activity.get({ id: $routeParams.id }, function (data) {
-           // $scope.activity.Date = moment($scope.activity.Date).format("YYYY-MM-DDTHH:mm");
-           // //$scope.activity.Date = new Date($scope.activity.Date);
-           // $scope.date = moment($scope.activity.Date).format("YYYY-MM-DD");
-            // $scope.time = moment($scope.activity.Date).format("HH:mm");
             $scope.time = moment($scope.activity.Date).format("HH:mm");
             $scope.activity.Date = data.Date.replace(/T.*/, "");
             $scope.activity.Date = Date.parse($scope.activity.Date);
@@ -49,17 +41,11 @@
 
         $scope.employees = Employee.query();
         $scope.edit = function () {
-            this.activity.Date = new Date($scope.date);
-            this.activity.Date.setHours(this.time.split(':')[0]);
-            this.activity.Date.setMinutes(this.time.split(':')[1]);
+            this.activity.Date.setUTCHours($scope.time.split(':')[0], $scope.time.split(':')[1]);
             $scope.activity.$save(function () {
                 $location.path('/');
             });
-        };
-
-        $scope.log = function () {
-            console.log($scope.activity);
-        };
+        };   
     }
 
     ActivitiesDeleteController.$inject = ['$scope', '$routeParams', '$location', 'Activity'];
